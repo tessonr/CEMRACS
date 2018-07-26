14f1de45e8c25351fccb6ce372d77d93599661d6
tessonr <remi.tesso@gmail.com>
2018-07-26 15:32:34 +0200

    Implementation diffusion

---
+++ Code_Macro/diffusion_operator.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse as sp
+
+def diffusion(Nx,Ny,dx,dy,D):
+    N=Nx*Ny
+    d0=(2/dx**2+2/dy**2)*np.ones(N)
+    d1=(-1/dx**2)*np.ones(N)
+    d2=(-1/dy**2)*np.ones(N)
+    A=sp.lil_matrix(sp.spdiags([d0,d1,d1,d2,d2],[0,1,-1,Nx,-Nx],N,N))
+    return A
+    
+# Nx=Ny=5
+# dx=dy=1
+# D=1
+# 
+# A=diffusion(Nx,Ny,dx,dy,D)
