1cb71cfa364b3237fd1a269b663cfa8dd740ac8d
tessonr <remi.tesso@gmail.com>
2018-07-26 16:08:55 +0200

    Link + D diffusion

--- Code_Macro/diffusion_operator.py
+++ Code_Macro/diffusion_operator.py
@@ -6,9 +6,9 @@ def diffusion(Nx,Ny,dx,dy,D):
     # Size of matrix
     N=Nx*Ny
     # Definitions of Diagonals
-    d0=(2/dx**2+2/dy**2)*np.ones(N)
-    d1=(-1/dx**2)*np.ones(N)
-    d2=(-1/dy**2)*np.ones(N)
+    d0=D*(2/dx**2+2/dy**2)*np.ones(N)
+    d1=D*(-1/dx**2)*np.ones(N)
+    d2=D*(-1/dy**2)*np.ones(N)
     # Definitions of diffusion matrix
     A=sp.lil_matrix(sp.spdiags([d0,d1,d1,d2,d2],[0,1,-1,Nx,-Nx],N,N))
     return A
