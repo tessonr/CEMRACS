56447e895de6cd569d923ca6c515a7a5a5a75fb5
atsoukevin93 <atsoukevin@gmail.com>
2018-08-02 16:05:05 +0200

    modification de la méthode de convolution dans la fonction main

--- Code_Macro/main.py
+++ Code_Macro/main.py
@@ -9,13 +9,12 @@ import link_operator as lop
 import phiST as ph
 import initial_condition as ic
 import time
-import matplotlib.animation as animation
 
 # Parameters for the model
 
 KAA=1.
-KAB=200.
-KBA=200.
+KAB=1.
+KBA=1.
 KBB=1.
 
 nuAAc=1.
@@ -57,8 +56,8 @@ X=np.zeros((2,M))
 X[0,:]=np.reshape(x,(M))
 X[1,:]=np.reshape(y,(M))
 
-dt=0.01
-T=1.
+dt=5e-3
+T=10.
 Nt=int(T/dt)
 
 th=2.
@@ -85,7 +84,29 @@ MatA=sp.csc_matrix(MatA)
 MatB=(MatT+dt*DB*MatD)
 MatA=sp.csc_matrix(MatB)
 
+#construction of the intra and inter species interaction potentials their FOurier Transform
 
+#for A
+argsAA=(KAA, nuAAc, nuAAd, R)
+argsAB=(KAB, nuABc, nuABd, R)
+phiAA = ph.phiST(X,argsAA)
+phiAB = ph.phiST(X,argsAB)
+#Fast Fourier Transform of phiAA and phiAB
+rphiAA = np.reshape(phiAA,(Nx,Ny))
+rphiAB = np.reshape(phiAB,(Nx,Ny))
+fftphiAA = np.fft.fft2(rphiAA)
+fftphiAB = np.fft.fft2(rphiAB)
+
+#for B
+argsBB=(KBB, nuBBc, nuBBd, R)
+argsBA=(KBA, nuBAc, nuBAd, R)
+phiBA = ph.phiST(X,argsBA)
+phiBB = ph.phiST(X,argsBB)
+#Fast Fourier Transform of phiBA and phiBB
+rphiBA = np.reshape(phiBA,(Nx,Ny))
+rphiBB = np.reshape(phiBB,(Nx,Ny))
+fftphiBA = np.fft.fft2(rphiAA)
+fftphiBB = np.fft.fft2(rphiAB)
 # Time scheme
 
 for i in range(Nt):
@@ -94,19 +115,21 @@ for i in range(Nt):
     
     # second membre
     # for A
-    argsAA=(KAA, nuAAc, nuAAd, R)
-    argsAB=(KAB, nuABc, nuABd, R)
-    LO_A=dt*lop.link_operator(dx,dy,Nx,Ny,X,th,fA,fB,argsAA,argsAB,ph.phiST,ph.phiST)
-    
+    # argsAA=(KAA, nuAAc, nuAAd, R)
+    # argsAB=(KAB, nuABc, nuABd, R)
+    # LO_A=dt*lop.link_operator(dx,dy,Nx,Ny,X,th,fA,fB,argsAA,argsAB,ph.phiST,ph.phiST)
+    LO_A=dt*lop.link_operator_fft(dx,dy,Nx,Ny,th,fA,fB,fftphiAA,fftphiAB)
+
     FR_A=dt*lo.logistic(fA,fB,fstar,nuA)
     
     vecA=fA+LO_A+FR_A
     
     # for B
-    argsBB=(KBB, nuBBc, nuBBd, R)
-    argsBA=(KBA, nuBAc, nuBAd, R)
-    LO_B=dt*lop.link_operator(dx,dy,Nx,Ny,X,th,fB,fA,argsBB,argsBA,ph.phiST,ph.phiST)
-    
+    # argsBB=(KBB, nuBBc, nuBBd, R)
+    # argsBA=(KBA, nuBAc, nuBAd, R)
+    # LO_B=dt*lop.link_operator(dx,dy,Nx,Ny,X,th,fB,fA,argsBB,argsBA,ph.phiST,ph.phiST)
+    LO_B=dt*lop.link_operator_fft(dx,dy,Nx,Ny,th,fA,fB,fftphiBA,fftphiBB)
+
     FR_B=dt*lo.logistic(fB,fA,fstar,nuB)
     
     vecB=fB+LO_B+FR_B
@@ -128,25 +151,10 @@ for i in range(Nt):
     FA[:,i+1]=fA
     FB[:,i+1]=fB
 
-# fig = plt.figure(3)
-# plt.contourf(x,y,np.reshape(fA,(Nx,Ny)),cmap=plt.cm.hot)
-# plt.colorbar()
-# plt.show()
-   
-t = np.arange(0., T,dt)
-fig = plt.figure(3)
 
-def update(iframe):
-    plt.clf()
-    fig.canvas.draw()
-    plt.subplot(121)
-    plt.contourf(x,y,np.reshape(FA[:,iframe],(Nx,Ny)),cmap=plt.cm.hot)
-    plt.colorbar()
-    
-    plt.subplot(122)
-    plt.contourf(x,y,np.reshape(FB[:,iframe],(Nx,Ny)),cmap=plt.cm.hot)
-    plt.colorbar()
-
-anim = animation.FuncAnimation(fig, update, frames=t.size, interval=1, repeat=True)
+plt.contourf(x,y,np.reshape(fA,(Nx,Ny)),cmap=plt.cm.hot)
+plt.colorbar()
 plt.show()
+   
+   
    
