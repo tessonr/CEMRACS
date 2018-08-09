94dce694f7854bddfb58fb9279e6bba719ca01b6
tessonr <remi.tesso@gmail.com>
2018-08-09 15:40:29 +0200

    debug initial condition

--- Code_Macro/initial_condition.py
+++ Code_Macro/initial_condition.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-def random_square(N,dx,dy):
-    f=np.ones(N)+np.random.uniform(0,1,N)
+def random_square(N,pert,dx,dy):
+    f=np.ones(N)+pert*np.random.uniform(0,1,N)
     f=f/(np.sum(f)*dx*dy)
     return f
     
 N=10
 dx=1
 dy=1
+pert=0.01
 
-f=random_square(N,dx,dy)
+f=random_square(N,pert,dx,dy)
